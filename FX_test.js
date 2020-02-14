f8cdaa03af07df514d5b7c1c313da9cdf6292102
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2020-02-14 19:45:16 +0700

    Add files via upload
    155
    *test for state "no roles"

--- FX_test.js
+++ FX_test.js
@@ -63,6 +63,9 @@ if(document.title != 'Inetcore+' && ((window.location.href.indexOf('https://fx.m
 		
 		window.AppInventor.setWebViewString('version_:FX_test_v155');
 		
+		document.getElementsByClassName('logo-inetcore')[0].addEventListener("click", sendClickLogo);
+		function sendClickLogo(){window.AppInventor.setWebViewString('string_1:logo-inetcore');}
+		
 		document.body.addEventListener("click", updateHTML);
 		
 		var usertext='';
