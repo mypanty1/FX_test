5317b40f8bdb9f1b90fdfcd96b03e6d8f20b7200
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2021-02-27 18:59:31 +0700

    Add files via upload

--- FX_test.js
+++ FX_test.js
@@ -2213,7 +2213,42 @@ if(document.title!='Inetcore+'&&(window.location.href.includes('https://fx.mts.r
 		},
 	});
 	
-	
+	/*времянка entry.log 30805111868*/
+	document.getElementById('session-logs-template').innerHTML=`
+		<modal-container ref='sessionLogs'>
+    <div class="mx-auto mt-8 w-75">
+      <h3 class="font--18-600 tone-900 d-center-x mb-8">
+        логи авторизации
+      </h3>
+    </div>
+    <div>
+
+      <div>
+        <div class='d-center-x px-16 py-16'>
+          <text-field-el :value='logs.date' label='дата' type='date'  v-model='logs.date'/>
+        </div>
+
+        <div class='px-16 pb-16'>
+          <button-main @click="load" :disabled="loading" label='загрузить' :loading='loading' size='full' buttonStyle='contained'></button-main>
+        </div>
+      </div>
+      <template v-if='logs.data' v-for="entry in logs.data">
+        <devider-line></devider-line>
+        <div class='py-16'>
+          <div class='font--13-500-140 tone-900 px-16'> {{ entry.logs }} ({{ entry.count }})</div>
+          <info-value :label='entry.enter_login' :value='entry.last' type='large' :whithLine=true></info-value>
+          <info-value label='MAC' :value='entry.mac' type='large' :whithLine=true></info-value>
+          <info-value label='NAS' :value='entry.nas' type='large' :whithLine=true></info-value>
+		  <span style="margin-left:1rem;">{{entry.log}}</span>
+        </div>
+      </template>
+    </div>
+
+    <div class='px-16' v-if="Array.isArray(logs.data) && logs.data.length == 0">
+      <message-el :text='"авторизаций "+logs.date+" не было"' :box='true' type='info'></message-el>
+    </div>
+  </modal-container>
+	  `;
 	
 	Vue.component('account-billing-modal',{/*потерян при редизайне*/
 		template:`
