f9329b98715f5eafa45352b105aa3deee1e66d3f
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2022-12-26 14:21:07 +0700

    Update FIX_test_11.12.22.js

--- FIX_test_11.12.22.js
+++ FIX_test_11.12.22.js
@@ -2798,9 +2798,9 @@ if(app.$store.getters['main/userData']?.username=='mypanty1'){
     document.body.insertAdjacentHTML(`afterbegin`,`<div id="particles-js-el" style="position:fixed;top:0;left:0;right:0;bottom:0;z-index:2;pointer-events:none;"></div>`);
     setTimeout(()=>particlesJS?particlesJS('particles-js-el',{
       particles:{
-        number:{value:400,density:{enable:true,value_area:800}},
+        number:{value:200,density:{enable:true,value_area:400}},
         color:{value:'#ffffff'},
-        shape:{type:'image',stroke:{width:3,color:'#fff'},polygon:{nb_sides:5},image:{src:`https://i.ibb.co/23nz2Rd/snowflake3-min.png`,width:100,height:100}},
+        shape:{type:'image',stroke:{width:3,color:'#fff'},polygon:{nb_sides:5},image:{src:`https://mypanty1.github.io/snow/snowflake3-min.png`,width:100,height:100}},
         opacity:{value:1,random:false,anim:{enable:false,speed:1,opacity_min:0.1,sync:false}},
         size:{value:10,random:true,anim:{enable:false,speed:20,size_min:1,sync:false}},
         line_linked:{enable:false,distance:50,color:'#ffffff',opacity:0.6,width:1},
