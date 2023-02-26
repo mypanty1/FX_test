a74db4b1f90567c72c573c2f869f148ef26347b0
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-02-26 12:35:03 +0700

    Update FIX_test_26.02.23.js

--- FIX_test_26.02.23.js
+++ FIX_test_26.02.23.js
@@ -3340,11 +3340,12 @@ Vue.component("port-logs-modal", {
       if(!this.filter){
         return this.log.data.filter(v=>v)//.slice(0,200)
       };
-      const ifName=` ${this.port.snmp_name} `;//Fiberhome, Huawei(+in=ev)
+      const ifName=` ${this.port.snmp_name} `;//Fiberhome, Huawei
       const poNumUp=`Port ${this.port.snmp_number} `;//D-Link
-      const poNumLo=`port ${this.port.snmp_number} `;//D-Link DES-1210-28/ME, DGS-1210-28X/ME/B1A
+      //const poNumLo=`port ${this.port.snmp_number} `;//D-Link DES-1210-28/ME, DGS-1210-28X/ME/B1A
       return this.log.data.filter(r=>{
-        return r.includes(ifName) || r.includes(poNumUp) || r.includes(poNumLo)
+        return new RegExp(`[^a-zA-z]${ifName}[^0-9]`).test(r) || new RegExp(`[^a-zA-z]${poNumUp}[^0-9]`,'i').test(row)
+        //return r.includes(ifName) || r.includes(poNumUp) || r.includes(poNumLo)
       });
     }
   },
