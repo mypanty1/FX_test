0f01e32804262569ac9f48e30ec9ad8a2384b7a9
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-03-09 01:36:55 +0700

    Update FIX_test_26.02.23.js

--- FIX_test_26.02.23.js
+++ FIX_test_26.02.23.js
@@ -125,6 +125,9 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 //port log indexing
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortActionsPortLogs.js',type:'text/javascript'}));
 
+//portsmap logs indexing
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortsMapLogs.js',type:'text/javascript'}));
+
 //user ip ping and go
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/DeviceActionPing.js',type:'text/javascript'}));
 
