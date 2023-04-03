8e9801a1eb3f3feb782fc10b5c111b29ed366c47
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-04-03 23:14:47 +0700

    Update FIX_test_24.03.23.js

--- FIX_test_24.03.23.js
+++ FIX_test_24.03.23.js
@@ -146,9 +146,8 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SendKionPq.js',type:'text/javascript'}));
 
 //cpe test 2
-if(!ACS_CPE){
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/AccountCpePage2.js',type:'text/javascript'}));
-};
+
 
 
 
