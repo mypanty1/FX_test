19e492651013d8969f3d6c412604239fab75d402
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-05-25 13:39:44 +0700

    Update SendKionPq.js

--- SendKionPq.js
+++ SendKionPq.js
@@ -173,7 +173,7 @@ Vue.component('SendKionPq',{
   }
 });
 
-Vue.component("LbsvAccountMain",{//add SendKionPq
+Vue.component("LbsvAccountMain", {//add SendKionPq
   template:`<CardBlock name="LbsvAccountMain" v-if="account">
     <title-main>
       <div slot="prefix">
@@ -182,27 +182,27 @@ Vue.component("LbsvAccountMain",{//add SendKionPq
       <span slot="text" class="display-flex align-items-center gap-5px">
         <span>{{accountId}}</span>
         <span v-if="agreement?.closedon" class="font--12-400 tone-500">расторгнут {{agreement.closedon}}</span>
-        <span v-if="agreement?.isconvergent&&!agreement?.closedon" class="lbsv-account__convergent">Конвергент</span>
+        <span v-if="!!msisdn&&!agreement?.closedon" class="lbsv-account__convergent">Конвергент</span>
       </span>
     </title-main>
     <devider-line/>
     <info-text-icon icon="" :text="formatedAddress" type="medium"/>
     <devider-line/>
     <title-main icon="person" :text="account.name" textClass="text-transform-capitalize"/>
-    <div v-if="agreement && phone === agreement.convergentmsisdn">
-      <account-call v-if="agreement?.isconvergent" :phone="agreement.convergentmsisdn" :isConvergent="agreement.isconvergent" class="margin-bottom-16px" showSendSms/>
+    <div v-if="agreement && phone === msisdn">
+      <account-call v-if="!!msisdn" :phone="msisdn" :isConvergent="!!msisdn" class="margin-bottom-16px" showSendSms/>
     </div>
     <div v-else>
-      <account-call v-if="agreement?.isconvergent" :phone="agreement.convergentmsisdn" :isConvergent="agreement.isconvergent" class="margin-bottom-16px" showSendSms/>
+      <account-call v-if="!!msisdn" :phone="msisdn" :isConvergent="!!msisdn" class="margin-bottom-16px" showSendSms/>
       <account-call v-if="phone" :phone="phone" class="margin-bottom-16px" showSendSms/>
     </div>
     
     <SendKionPq :phone="phone" :phones="[account?.mobile,account?.phone,agreement?.convergentmsisdn]" :account="accountId"/>
-    
+
     <devider-line v-if="agreement"/>
     <template v-if="agreement">
       <info-value icon="purse" :label="balanceLabel" :value="balance" :minus="agreement.balance.minus" type="extra"/>
-      <info-value icon="purse" v-if="agreement?.convergentmsisdn && convergentBalance" :label="balanceLabelConvergent" :value="convergentBalance+' ₽'" :minus="convergentBalance < 0" type="extra"/>
+      <info-value icon="purse" v-if="msisdn && convergentBalance" :label="balanceLabelConvergent" :value="convergentBalance+' ₽'" :minus="convergentBalance < 0" type="extra"/>
       <info-value icon="clock" v-if="agreement.lastpaydate" label="Платеж" :value="lastPayText" type="extra"/>
     </template>
     <devider-line/>
@@ -216,9 +216,16 @@ Vue.component("LbsvAccountMain",{//add SendKionPq
     flatData:{type:Object,default:null},
     loading:{type:Object,required:true},
     billingInfo:{type:Array,default:()=>[]},
-    convergentBalance:{type:[String,Number],default:null},
+    //convergentBalance:{type:[String,Number],default:null},
     accountId:{type:String,default:''},
     flat:{type:Object,default:null}
+  },
+  data:()=>({
+    msisdn:'',
+    convergentBalance:null,
+  }),
+  created(){
+    this.getConvergent();
   },
   computed: {
     addr_type2(){//0-прописки, 1-проживания, 2-доставки счетов
@@ -255,11 +262,37 @@ Vue.component("LbsvAccountMain",{//add SendKionPq
     },
     phone(){return this.account.mobile||this.account.phone},
     balanceLabel(){return `Баланс (ЛС ${this.accountId})`},
-    balanceLabelConvergent(){return `Баланс (+${this.agreement.convergentmsisdn})`},
+    balanceLabelConvergent(){return `Баланс (+${this.msisdn})`},
   },
   methods: {
     openSendSmsModal() {
       this.$refs.sendSms.open();
+    },
+    async getConvergent(){
+      await this.getMsisdn();
+      if(this.msisdn){
+        this.getConvergentBalance();
+      };
+    },
+    async getMsisdn(){
+      const {agreement:{account}={}}=this;
+      if(!account){return};
+      try{
+        const response=await httpGet(buildUrl("get_user_phone",{agrm_num:account},"/call/sri/"));
+        this.msisdn = response?.return?.indexOf('not found') === -1 ? response.return : '';
+      }catch(error){
+        console.warn("get_user_phone.error",error);
+      };
+    },
+    async getConvergentBalance(){
+      const {msisdn}=this;
+      if(!msisdn){return};
+      try{
+        const response=await httpGet(buildUrl("convergent",{msisdn},"/call/v1/foris/"));
+        this.convergentBalance=response.balance;
+      }catch (error){
+        console.warn("convergent.error",error);
+      };
     },
   },
 });
