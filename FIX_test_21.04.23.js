fe9f7a357ce05ac894d0f6b7a5e6a98752278a9b
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-06-30 18:18:46 +0700

    Update FIX_test_21.04.23.js

--- FIX_test_21.04.23.js
+++ FIX_test_21.04.23.js
@@ -152,8 +152,8 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 //add cpu/mem
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CpeSection3.js',type:'text/javascript'}));
 
-//LocalNotes
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/task_LocalNotes.js',type:'text/javascript'}));
+//SendKionPq_LocalNotes_SibelService
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/task_SendKionPq_LocalNotes_SibelService.js',type:'text/javascript'}));
 
 
 
