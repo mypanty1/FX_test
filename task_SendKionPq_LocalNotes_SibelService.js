53444e5337792defcdc811ae6b6804b0257e49c2
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-06-30 20:59:28 +0700

    Update task_SendKionPq_LocalNotes_SibelService.js

--- task_SendKionPq_LocalNotes_SibelService.js
+++ task_SendKionPq_LocalNotes_SibelService.js
@@ -487,6 +487,160 @@ Vue.component('task-main-account',{
   },
 });
 
+//add SendKionPq and SibelServiceRequests
+Vue.component("LbsvAccountMain300523", {
+  template:`<CardBlock name="LbsvAccountMain300523" v-if="account">
+    <title-main>
+      <div slot="prefix">
+        <!--<span class="ic-20 ic-status" :class="!agreement.closedon?'main-green':'main-red'"></span>-->
+        <IcIcon name="status" :class="!agreement.closedon?'main-green':'main-red'"/>
+      </div>
+      <span slot="text" class="display-flex align-items-center gap-5px">
+        <span>{{accountId}}</span>
+        <span v-if="agreement?.closedon" class="font--12-400 tone-500">расторгнут {{agreement.closedon}}</span>
+        <span v-if="!!msisdn&&!agreement?.closedon" class="lbsv-account__convergent">Конвергент</span>
+      </span>
+    </title-main>
+    <devider-line/>
+    <info-text-icon icon="" :text="formatedAddress" type="medium"/>
+    <devider-line/>
+    <title-main icon="person" :text="account.name" textClass="text-transform-capitalize"/>
+    <div v-if="agreement && phone === msisdn">
+      <account-call v-if="!!msisdn" :phone="msisdn" :isConvergent="!!msisdn" class="margin-bottom-16px" showSendSms/>
+    </div>
+    <div v-else>
+      <account-call v-if="!!msisdn" :phone="msisdn" :isConvergent="!!msisdn" class="margin-bottom-16px" showSendSms/>
+      <account-call v-if="phone" :phone="phone" class="margin-bottom-16px" showSendSms/>
+    </div>
+    
+    <SendKionPq :phone="phone" :phones="[account?.mobile,account?.phone,msisdn]" :account="agreement?.account||accountId"/>
+    
+    <devider-line v-if="agreement"/>
+    <template v-if="agreement">
+      <info-value icon="purse" :label="balanceLabel" :value="balance" :minus="agreement.balance.minus" type="extra"/>
+      <info-value icon="purse" v-if="msisdn && convergentBalance" :label="balanceLabelConvergent" :value="convergentBalance+' ₽'" :minus="convergentBalance < 0" type="extra"/>
+      <info-value icon="clock" v-if="agreement.lastpaydate" label="Платеж" :value="lastPayText" type="extra"/>
+    </template>
+    <devider-line/>
+    <SibelServiceRequests :agrNumber="agreement?.account||accountId"/>
+    <devider-line/>
+    <link-block icon="sms" text="Смс с новым паролем" @block-click="openSendSmsModal" action-icon="expand"/>
+    <send-sms-modal ref="sendSms" :account="accountId"/>
+    <template v-if="favBtnProps">
+      <devider-line/>
+      <FavBtnLinkBlock v-bind="favBtnProps"/>
+    </template>
+  </CardBlock>`,
+  props:{
+    account:{type:Object,default:null},
+    agreement:{type:Object,default:null},
+    flatData:{type:Object,default:null},
+    loading:{type:Object,required:true},
+    billingInfo:{type:Array,default:()=>[]},
+    //convergentBalance:{type:[String,Number],default:null},
+    accountId:{type:String,default:''},
+    flat:{type:Object,default:null}
+  },
+  data:()=>({
+    msisdn:'',
+    convergentBalance:null,
+  }),
+  created(){
+    this.getConvergent();
+  },
+  computed: {
+    favBtnProps(){
+      if(!this.account||!this.agreement){return};
+      const {agreement,agreement:{account},msisdn}=this;
+      return {
+        title:`Абонент ${truncateSiteAddress(this.formatedAddress).replace(/(, )/,',')}`,
+        name:account,
+        id:account,
+        path:`/${account}`,//если открыт из наряда
+        descr:[
+          objectToTable(filterKeys(this.account,{
+            '!1':['ЛС',account],
+            name:'Абонент|-',
+            mobile:['Тлф',(m,account)=>account.mobile||account.phone],
+            '!2':['Конвергент',msisdn||'-']
+          })),
+          objectToTable(filterKeys(this.agreement,Object.values(agreement.services||{}).map(({vgroups=[]}={})=>vgroups||[]).flat().reduce((services,service)=>{
+            if(!service||service.status=='10'){return services};
+            services[service.vgid]=[`${service.serviceclassname} [${service.vgid}]`,`${([1,4,5,6].includes(service.billing_type)?`${service.login} `:'')}${service.agentdescr}`]
+            return services;
+          },{})))
+        ].join('\n'),
+      }
+    },
+    addr_type2(){//0-прописки, 1-проживания, 2-доставки счетов
+      if(!this.account?.vgroups?.[0]||!this.agreement){return ""};
+      const addresses=this.account.vgroups.find(service=>service.agrmid==this.agreement.agrmid&&service.addresses.find(address=>address?.type==2))?.addresses||[];
+      const addr_type2=addresses?.find(address=>address?.type==2)?.address;
+      return addr_type2||''
+    },
+    computedAddress() {
+      if (!this.account) return "";
+      if (this.agreement) {
+        const service = this.account.vgroups.find((s) => s.agrmid == this.agreement.agrmid && s.connaddress);
+        if (service) return service.vgaddress || service.connaddress;
+      }
+      let address = {};
+      if (Array.isArray(this.account.addresses)) {
+        address = this.account.addresses.find((a) => a.address) || {};
+      }
+      return this.account.address || address.address || "";
+    },
+    formatedAddress(){
+      const address=this.addr_type2||this.computedAddress;
+      if(!address){return ''};
+      return address.split(',').map(elem=>elem.trim()).filter(v=>v).join(", ")
+    },
+    balance(){
+      const {balance}=this.agreement;
+      return `${balance.minus?'-':''}${balance.integer}.${balance.fraction} ₽`;
+    },
+    lastPayText(){
+      const lastsum=this.agreement.lastsum||'';
+      const lastpaydate=this.agreement.lastpaydate||'';
+      return `${lastsum} ${lastsum?'₽':''} ${lastpaydate?'•':''} ${lastpaydate}`;
+    },
+    phone(){return this.account.mobile||this.account.phone},
+    balanceLabel(){return `Баланс (ЛС ${this.accountId})`},
+    balanceLabelConvergent(){return `Баланс (+${this.msisdn})`},
+  },
+  methods: {
+    openSendSmsModal() {
+      this.$refs.sendSms.open();
+    },
+    async getConvergent(){
+      await this.getMsisdn();
+      if(this.msisdn){
+        this.getConvergentBalance();
+      };
+    },
+    async getMsisdn(){
+      const {agreement:{account}={}}=this;
+      if(!account){return};
+      try{
+        const response=await httpGet(buildUrl("get_user_phone",{agrm_num:account},"/call/sri/"));
+        this.msisdn = response?.return?.indexOf('not found') === -1 ? response.return : '';
+      }catch(error){
+        console.warn("get_user_phone.error",error);
+      };
+    },
+    async getConvergentBalance(){
+      const {msisdn}=this;
+      if(!msisdn){return};
+      try{
+        const response=await httpGet(buildUrl("convergent",{msisdn},"/call/v1/foris/"));
+        this.convergentBalance=response.balance;
+      }catch (error){
+        console.warn("convergent.error",error);
+      };
+    },
+  },
+});
+
 //siebel components
 Vue.component("ServiceRequestCommentsModal", {
   template:`<modal-container-custom name="ServiceRequestCommentsModal" ref="modal" :footer="false" :wrapperStyle="{'min-height':'auto','margin-top':'4px'}">
@@ -662,7 +816,7 @@ Vue.component('SibelServiceRequest',{
 });
 
 Vue.component('SibelServiceRequests',{
-  template:`<DropdownBlock name="SibelServiceRequests" :title="{icon:'info',text:'Другие обращения абонента',text2:serviceRequestsFilteredCount?serviceRequestsFilteredCount:'',text2Class:'tone-500'}">
+  template:`<DropdownBlock name="SibelServiceRequests" :title="{icon:'info',text:title,text2:serviceRequestsFilteredCount?serviceRequestsFilteredCount:'',text2Class:'tone-500'}">
     <div class="margin-left-right-16px">
       <loader-bootstrap v-if="loading" text="получение недавних обращений"/>
       <message-el v-if="!serviceRequestsFilteredCount" text="Нет недавних обращений" box type="info"/>
@@ -686,6 +840,9 @@ Vue.component('SibelServiceRequests',{
     this.getServiceRequests();
   },
   computed:{
+    title(){
+      return `${this.srNumberCurrent?'Другие обращения':'Обращения'} абонента`
+    },
     agreementNum(){
       const {agrNumber}=this;
       return agrNumber.match(/\d/g)?.join('')||'';
@@ -695,7 +852,7 @@ Vue.component('SibelServiceRequests',{
       return srNumberCurrent?(serviceRequests||[]).filter(({number})=>number!==srNumberCurrent):serviceRequests;
     },
     serviceRequestsFilteredCount(){
-      return this.serviceRequestsFiltered.length;
+      return this.serviceRequestsFiltered?.length||0;
     }
   },
   methods:{
