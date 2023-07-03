7c6ffd207b604ca0729b50f6662a13f2fabe0510
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-07-04 01:58:47 +0700

    Update FIX_test_21.04.23.js

--- FIX_test_21.04.23.js
+++ FIX_test_21.04.23.js
@@ -158,6 +158,8 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 //add ifLastChange to PortLayout
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortLayout2_ifLastChange.js',type:'text/javascript'}));
 
+//ref Foris
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/Foris.js',type:'text/javascript'}));
 
 
 
