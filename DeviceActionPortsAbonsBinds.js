34192e94342af5341be6ea24d64bb83cbf3e7bf3
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-07-21 01:14:52 +0700

    Update DeviceActionPortsAbonsBinds.js

--- DeviceActionPortsAbonsBinds.js
+++ DeviceActionPortsAbonsBinds.js
@@ -61,19 +61,27 @@ Vue.component('device-actions',{
 
 Vue.component('DeviceActionPortsAbonsBinds',{
   template:`<div name="DeviceActionPortsAbonsBinds">
-    <div class="site-add-table">
-      <div bb tac>лс</div><div bb>кв</div><div bb>порт</div><div bb>статус</div>
-      <div></div><div></div><div></div><div></div>
-      <template v-for="({port,flat,account,status},index) in rows">
-        <div style="white-space:pre;">{{account}}</div>
+    <div class="fix-binds-table">
+      <template>
+        <div bb br>порт</div>
+        <div bb br class="min-width-32px text-align-center">кв</div>
+        <div bb br class="min-width-90px text-align-center">лс</div>
+        <div bb class="min-width-45px text-align-center">статус</div>
+      </template>
+      <template>
+        <div></div><div></div><div></div><div></div>
+      </template>
+      <template v-for="({port,ifName,flat,account,status,message,error,statusStyle},key) in rows">
+        <div class="white-space-pre">{{ifName||port}}</div>
         <div>{{flat}}</div>
-        <div>{{port}}</div>
-        <div>{{status}}</div>
+        <div class="white-space-pre">{{account}}</div>
+        <div class="white-space-pre text-align-center" :style="statusStyle">{{status}}</div>
+        <div v-if="message||error" full class="white-space-pre margin-bottom-4px bg-attention-warning border-radius-4px">{{message||error}}</div>
       </template>
     </div>
     <div style="display:inline-flex;gap:4px;width:100%;justify-content:center;margin-top:8px;">
-      <button-main @click="clear" label="clear" :disabled="!is54||!isETH||loadingSome" buttonStyle="outlined" size="medium"/>
-      <button-main @click="start" label="start" :loading="loadingSome" :disabled="!is54||!isETH||loadingSome" buttonStyle="contained" size="medium"/>
+      <button-main @click="clear" label="clear" :disabled="!isValidRegion||!isETH||loadingSome" buttonStyle="outlined" size="medium"/>
+      <button-main @click="start" label="start" :loading="loadingSome" :disabled="!isValidRegion||!isETH||loadingSome" buttonStyle="contained" size="medium"/>
     </div>
   </div>`,
   props:{
@@ -90,8 +98,45 @@ Vue.component('DeviceActionPortsAbonsBinds',{
     results:{},
     loading:false
   }),
+  mounted(){
+    (function(id='DeviceActionPortsAbonsBinds'){
+      document.getElementById(id)?.remove();
+      const el=Object.assign(document.createElement('style'),{type:'text/css',id});
+      el.appendChild(document.createTextNode(`
+        .fix-binds-table{
+          position:relative;
+          display:grid;
+          grid-template-columns:1fr repeat(3,max-content);
+          min-height:50px;
+          border:solid 1px #c8c7c7;
+          border-radius:6px;
+          padding:0px 8px;
+          max-width:400px;
+          gap:2px;
+        }
+        .fix-binds-table>div{
+          color:#221e1e;
+          font-size:12px;
+          line-height:16px;
+          padding-left: 2px;
+          padding-right: 2px;
+        }
+        .fix-binds-table>div[full]{
+          grid-column: span 4;
+        }
+        .fix-binds-table>div[bb]{
+          border-bottom:solid 1px #c8c7c7
+        }
+        .fix-binds-table>div[br]{
+          border-right:solid 1px #c8c7c7
+        }
+      `));
+      document.body.insertAdjacentElement('afterBegin',el);
+    }());
+  },
   computed:{
-    is54(){return this.networkElement?.region?.id==54},
+    //is54(){return this.networkElement?.region?.id==54},
+    isValidRegion(){return [54,58].includes(this.networkElement?.region?.id)},
     name(){return this.networkElement.name},
     isETH(){return /^eth/i.test(this.name||'')},
     ip(){return this.networkElement.ip},
@@ -101,9 +146,11 @@ Vue.component('DeviceActionPortsAbonsBinds',{
       return Object.entries(this.results).reduce((rows,[_account,result])=>{
         const cp=this.abons[_account]?.cp;if(!cp){return rows};
         const {port,flat,mac,account,ifName}=cp;
+        const {message,error,success,loading}=result;
         rows[port]={
-          port,flat,mac,account,ifName,
-          status:result.success?'success ✔':result.error?'error ✘':result.loading?'loading...':'',
+          port,flat,mac,account,ifName,message,error,
+          status:success?'✔':error?'✘':loading?'...':'',
+          statusStyle:success?'font-weight:700;color:#478f22;':error?'font-weight:700;color:#d17a60;':loading?'font-weight:900;color:#5642bd;':''
         }
         return rows
       },{});
@@ -113,7 +160,7 @@ Vue.component('DeviceActionPortsAbonsBinds',{
     async start(){
       await this.getDevicePorts();
       this.loading=true;
-      for(const {number:port,flat,subscriber_list=[],state,snmp_name:ifName} of this.ports){
+      for(const {number:port,flat,subscriber_list=[],state='',snmp_name:ifName=''/*,last_mac:{value:mac=''}={}*/} of this.ports){
         if(!subscriber_list.length||state!=='busy'){continue};
         const subscriber=subscriber_list.find(({account,flat:_flat})=>_flat==flat&&account);
         if(!subscriber?.account){
@@ -133,21 +180,21 @@ Vue.component('DeviceActionPortsAbonsBinds',{
             this.$set(this.results,account,{error:'agreement error'});
             continue
           };
-          const {serverid,type_of_bind,vgid}=(agreement?.services?.internet?.vgroups||[]).find(({status})=>['0','1','2','3'].includes(status))||{};
-          if(!serverid||!type_of_bind||!vgid){
-            console.warn({port,flat,mac,account,serverid,type_of_bind,vgid,error:'service error',step:'service_vgid'});
+          const {serverid,type_of_bind,vgid,login}=(agreement?.services?.internet?.vgroups||[]).find(({status})=>['0','1','2','3'].includes(status))||{};
+          if(!serverid||!type_of_bind||!vgid||!login){
+            console.warn({port,flat,mac,account,serverid,type_of_bind,vgid,login,error:'service error',step:'service_vgid'});
             this.$set(this.results,account,{error:'service error'});
             continue
           };
           const {ip}=this;
           try{
-            const response_set_bind=await httpPost(`/call/service_mix/set_bind`,{ip,port,vgid,serverid,type_of_bind});
+            const response_set_bind=await httpPost(`/call/service_mix/set_bind`,{ip,port,vgid,login,serverid,type_of_bind});
             if(response_set_bind?.type=='error'){
               if(response_set_bind?.text?.length>0&&response_set_bind.text.indexOf('Мы не можем отобрать порт у контракта ')>=0){
                 let contract=parseInt(response_set_bind.text.replace('Мы не можем отобрать порт у контракта ',''));
                 if(!contract){
-                  console.warn({port,flat,mac,account,serverid,type_of_bind,vgid,contract,error:response_set_bind.text||'contract error',step:'port_contract'});
-                  this.$set(this.results,account,{error:'contract error'});
+                  console.warn({port,flat,mac,account,serverid,type_of_bind,vgid,login,contract,error:response_set_bind.text||'contract error',step:'port_contract'});
+                  this.$set(this.results,account,{error:'contract error',message:response_set_bind?.text});
                   continue
                 };
                 contract=contract.toString();
@@ -156,14 +203,14 @@ Vue.component('DeviceActionPortsAbonsBinds',{
                   const response_refree=await httpPost(`/call/service_mix/set_bind`,{ip,port:contract,vgid:contract,serverid,type_of_bind});
                   if(response_refree?.type=='error'){
                     console.warn({port,flat,mac,account,serverid,type_of_bind,vgid,contract,error:response_refree?.text||'refree error',step:'port_refree_vgid'});
-                    this.$set(this.results,account,{error:'refree error'});
+                    this.$set(this.results,account,{error:'refree error',message:response_refree?.text});
                     continue
                   }else{
                     try{
                       const response_set_bind_2=await httpPost(`/call/service_mix/set_bind`,{ip,port,vgid,serverid,type_of_bind});
                       if(response_set_bind_2?.type=='error'){
                         console.warn({name,ip,port,flat,mac,account,serverid,type_of_bind,vgid,contract,error:response_set_bind_2?.text||'set_bind 2 error',step:'port_bind_vgid_2'});
-                        this.$set(this.results,account,{error:'set_bind 2 error'});
+                        this.$set(this.results,account,{error:'set_bind 2 error',message:response_set_bind_2?.text});
                         continue
                       }else{
                         console.log({port,flat,mac,account,serverid,type_of_bind,vgid,contract,success:true,step:'port_bind_vgid_2'});
@@ -179,11 +226,11 @@ Vue.component('DeviceActionPortsAbonsBinds',{
                   this.$set(this.results,account,{error:'unexpected refree error'});
                 }
               }else{
-                console.warn({port,flat,mac,account,serverid,type_of_bind,vgid,contract,error:response_set_bind_2?.text||'set_bind error',step:'port_bind_vgid'});
-                this.$set(this.results,account,{error:'set_bind error'});
+                console.warn({port,flat,mac,account,serverid,type_of_bind,vgid,login,error:response_set_bind?.text||'set_bind error',step:'port_bind_vgid'});
+                this.$set(this.results,account,{error:'set_bind error',message:response_set_bind?.text});
               }
             }else{
-              console.log({port,flat,mac,account,serverid,type_of_bind,vgid,success:true,step:'port_bind_vgid'});
+              console.log({port,flat,mac,account,serverid,type_of_bind,vgid,login,success:true,step:'port_bind_vgid'});
               this.$set(this.results,account,{success:true});
             };
           }catch(error){
@@ -227,89 +274,6 @@ Vue.component('DeviceActionPortsAbonsBinds',{
     }
   },
 });
-
-
-
-/*
-(async function(name='ETH_54KR_00566_1'){
-  const response_device=await httpGet("/call/v1/search/search_ma?pattern="+name);
-  if(!response_device?.data?.ip){
-    console.log({name,error:response_device?.text||'device error',step:'device'});
-    return
-  };
-  const {ip}=response_device.data;
-  const response_device_port_list=await httpGet("/call/device/device_port_list?device="+name);
-  if(!Array.isArray(response_device_port_list)||!response_device_port_list?.length){
-    console.warn({name,ip,error:'device_port_list error',step:'device_port_list'});
-    return
-  };
-  for(const {number:port,flat,subscriber_list=[],state} of response_device_port_list){
-    if(!subscriber_list.length||state!=='busy'){continue};
-    const subscriber=subscriber_list.find(({account,flat:_flat})=>_flat==flat&&account);
-    if(!subscriber?.account){
-      console.warn({name,ip,port,flat,error:'subscriber error',step:'port_subscriber'});
-      continue
-    };
-    const {mac,account}=subscriber;
-    
-    const abon_lbsv=await httpGet("/call/v1/search/search_ma?pattern="+account);
-    if(!abon_lbsv?.data?.lbsv?.data?.agreements){return};
-    const {agreements=[]}=abon_lbsv.data.lbsv.data;
-    const agreement=agreements.find(agreement=>agreement.account==account);
-    if(!agreement){
-      console.warn({name,ip,port,flat,mac,account,error:'agreement error',step:'abon_agreement'});
-      continue
-    };
-    const {serverid,type_of_bind,vgid}=(agreement?.services?.internet?.vgroups||[]).find(({status})=>['0','1','2','3'].includes(status))||{};//[актив,баланс,абон,админ]
-    if(!serverid||!type_of_bind||!vgid){
-      console.warn({name,ip,port,flat,mac,account,serverid,type_of_bind,vgid,error:'service error',step:'service_vgid'});
-      continue
-    };
-    
-    const response_set_bind=await httpPost(`/call/service_mix/set_bind`,{ip,port,vgid,serverid,type_of_bind});
-    if(response_set_bind?.type=='error'){
-      if(response_set_bind?.text?.length>0&&response_set_bind.text.indexOf('Мы не можем отобрать порт у контракта ')>=0){
-        let contract=parseInt(response_set_bind.text.replace('Мы не можем отобрать порт у контракта ',''));
-        if(!contract){
-          console.warn({name,ip,port,flat,mac,account,serverid,type_of_bind,vgid,contract,error:response_set_bind.text||'contract error',step:'port_contract'});
-          continue
-        };
-        contract=contract.toString();
-        
-        const response_refree=await httpPost(`/call/service_mix/set_bind`,{ip,port:contract,vgid:contract,serverid,type_of_bind});
-        if(response_refree?.type=='error'){
-          //освободить не удалось
-          console.warn({name,ip,port,flat,mac,account,serverid,type_of_bind,vgid,contract,error:response_refree?.text||'refree error',step:'port_refree_vgid'});
-          continue
-        }else{
-          //порт освобожден! response_refree?.Data?.IP
-          const response_set_bind_2=await httpPost(`/call/service_mix/set_bind`,{ip,port,vgid,serverid,type_of_bind});
-          if(response_set_bind_2?.type=='error'){
-            console.warn({name,ip,port,flat,mac,account,serverid,type_of_bind,vgid,contract,error:response_set_bind_2?.text||'set_bind 2 error',step:'port_bind_vgid_2'});
-            continue
-          }else{
-            console.log({name,ip,port,flat,mac,account,serverid,type_of_bind,vgid,contract,success:true,step:'port_bind_vgid_2'});
-          };
-        };
-      }else{
-        console.warn({name,ip,port,flat,mac,account,serverid,type_of_bind,vgid,contract,error:response_set_bind_2?.text||'set_bind error',step:'port_bind_vgid'});
-      }
-    }else{
-      console.log({name,ip,port,flat,mac,account,serverid,type_of_bind,vgid,success:true,step:'port_bind_vgid'});
-    };
-  }
-}())
-*/
-
-//device_ports_list.map(({number,flat,subscriber_list:[{account,mac}={}]})=>({number,flat,account,mac})).filter(({flat,account,mac})=>Number.isInteger(flat)&&account&&mac).map(({number,flat,account,mac})=>[mac,`kv${flat}-${(account||'').replace(/-/g,'')}-${(mac||'').replace(/:/g,'')?.slice(8,12)}-15.03.23`])
-
-//Object.values(device_ports).map(({ifName,ifAlias})=>[ifName,ifAlias]).filter(([ifName,ifAlias])=>ifAlias)
-
-
-
-
-
-
 
 
 
