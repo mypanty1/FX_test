2daa5fecd996b806d769298001b7097f00679774
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-07-22 14:25:29 +0700

    Update PortLastChange.js

--- PortLastChange.js
+++ PortLastChange.js
@@ -1,7 +1,8 @@
 //
-
 Vue.component('PortLastChange',{
-  template:`<div name="PortLastChange" class="display-contents"></div>`,
+  template:`<div name="PortLastChange" class="display-contents">
+    <info-text-sec v-if="ifLastChange" :title="ifLastChangeText"/>
+  </div>`,
   props:{
     port:{type:Object},
     networkElement:{type:Object},
@@ -9,6 +10,19 @@ Vue.component('PortLastChange',{
   },
   data:()=>({}),
   created(){},  
-  computed:{},
+  computed:{
+    ifLastChange(){
+      if(!this.status){return};
+      const {uptime_instance,last_change}=this.status;
+      if(!last_change){return};
+      const now=Date.now();
+      return new Date(Date.now()-uptime_instance*1000).toLocaleString();
+    },
+    ifLastChangeText(){
+      if(!this.status){return};
+      const {ifLastChange,status:{IF_OPER_STATUS}}=this;
+      return ifLastChange?`${IF_OPER_STATUS?'LinkUp':'LinkDown'} at ${ifLastChange}`:'';
+    },
+  },
   methods:{},
 });
