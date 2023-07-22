d2396fb148a2c615053bc944ca020f2128af532f
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-07-22 14:22:39 +0700

    Create PortLastChange.js

---
+++ PortLastChange.js
@@ -0,0 +1,14 @@
+//
+
+Vue.component('PortLastChange',{
+  template:`<div name="PortLastChange" class="display-contents"></div>`,
+  props:{
+    port:{type:Object},
+    networkElement:{type:Object},
+    status:{type:Object},
+  },
+  data:()=>({}),
+  created(){},  
+  computed:{},
+  methods:{},
+});
