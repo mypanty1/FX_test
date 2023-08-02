5100ecd9df88e6067a23959d1d95451c15a584bc
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-08-03 02:05:29 +0700

    Update SideBarExtModal.js

--- SideBarExtModal.js
+++ SideBarExtModal.js
@@ -1,17 +1,27 @@
-Vue.component('SideBarExtModal',{
-  //menu-sidebar
-  _template:`<div @click="$refs.SideBarExtModal.open()" class="display-flex align-items-center gap-8px">
-                  <IcIcon name="fas fa-grip-horizontal" color="#2139b8" class="font-size-22px"/>
-                  <span>Дополнительно</span>
-                </div>
-                <SideBarExtModal ref="SideBarExtModal"/>`,
-  template:`<modal-container-custom name="SideBarExtModal" ref="modal" :footer="false" :wrapperStyle="{'min-height':'auto','margin-top':'4px'}">
+Vue.component('SideBarExtModal2',{
+  template:`<modal-container-custom name="SideBarExtModal2" ref="modal" :footer="false" :wrapperStyle="{'min-height':'auto','margin-top':'4px'}">
     <div class="padding-left-right-16px">
       <div class="display-flex flex-direction-column gap-8px">
         <div class="font--15-600 text-align-center">Дополнительно</div>
         
         <div class="display-flex flex-direction-column">
+          <select-el ref="Selector" label="Виджеты" :items="widgets" itemKey="name" @input="addItem" clearable/>
+          <devider-line v-if="items.length"/>
           
+          <div class="display-flex flex-direction-column gap-8px">
+            <template v-for="(item,index) of items">
+              <SectionBorder class="padding-8px" :key="index">
+                <div class="display-flex justify-content-space-between gap-8px">
+                  <div class="font--13-500">{{item.name}}</div>
+                  <button-sq icon="close-1" @click="items.splice(index,1)" type="medium"/>
+                </div>
+                <component :key="item.name" :is="item.is" v-bind="item.props" ref="Widgets"/>
+              </SectionBorder>
+            </template>
+            <div v-if="!items.length" class="font--16-500 tone-300 text-align-center height-100px display-flex flex-direction-column justify-content-center">
+              Добавить виджет
+            </div>
+          </div>
         </div>
       </div>
       <div class="margin-top-16px display-flex justify-content-space-around">
@@ -20,10 +30,13 @@ Vue.component('SideBarExtModal',{
     </div>
   </modal-container-custom>`,
   data:()=>({
-    
+    widgets:[
+      {name:'Пинг СЭ',is:'WidgetPing'},
+    ],
+    items:[]
   }),
   computed:{
-    
+    someSelected(){return !isEmpty(this.items)},
   },
   methods:{
     open(){//public
@@ -31,6 +44,114 @@ Vue.component('SideBarExtModal',{
     },
     close(){//public
       this.$refs.modal.close();
+      this.$refs.Widgets.forEach(widget=>widget?.close())
     },
+    addItem(item){
+      if(item){
+        this.items.push(item)
+        this.$refs.Selector.clear()
+      };
+    }
+  },
+})
+
+Vue.component('WidgetPing',{
+  template:`<div name="WidgetPing">
+    <input-el placeholder="10.221.xxx.xxx" :label="label" v-model="ip" :disabled="enable" class="margin-bottom-8px">
+      <!--<button-sq slot="postfix" icon="right-link" @click="$router.push({name:'search',params:{text:'@D_IP:'+ip}})" v-if="ip"/>-->
+      <div v-if="valid" slot="postfix" class="margin-left-right-8px display-flex">
+        <PingLed v-bind="{ip,mr_id}"/>
+      </div>
+    </input-el>
+    <info-value :label="'получено: '+received" :value="'потеряно: '+lost" type="small"/>
+    <device-params-item-history :paramDays="pings" :item="config" :limit="count" chartClass="-" chartStyle="border:1px solid #e4e3e3;border-radius:5px;"/>
+    <div class="display-flex gap-4px justify-content-center">
+      <button-main @click="clear" label="clear" buttonStyle="outlined" size="medium"/>
+      <button-main @click="start" label="start" :loading="running" :disabled="!valid||enable||running" buttonStyle="contained" size="medium"/>
+      <button-main @click="abort" label="abort" buttonStyle="outlined" size="medium"/>
+    </div>
+  </div>`,
+  data:()=>({
+    ip:'',
+    config:{
+      param:'ping',
+      unit:'ms',
+    },
+    timer:undefined,
+    enable:false,
+    timeout:1000,
+    count:0,
+    running:false,
+    pings:[],
+  }),
+  watch:{
+    'ip'(ip){
+      if(!ip){return}
+      this.pings=[];
+    },
+  },
+  computed:{
+    ...mapGetters({
+      region:'main/region',
+      mr_id:'main/mr_id',
+      mr:'main/mr',
+    }),
+    label(){return this.mr?`IP (МР ${this.mr.name})`:`IP`},
+    valid(){return this.mr_id&&this.ip.split('.').length===4},
+    received(){return this.pings.filter(ping=>ping.values[0]>=0).length},
+    lost(){return this.pings.filter(ping=>ping.values[0]<0).length},
+  },
+  methods:{
+    ...mapActions({
+      doPing:'dnm/doPing',
+    }),
+    async ping(){
+      this.running=true;
+      try{
+        const {mr_id,ip}=this;
+        const result=await this.doPing({mr_id,ip});
+        this.running=false;
+        this.count++;
+        this.pings.push({
+          date:this.count,
+          values:[result.state=='online'?result.ms:-2]
+        });
+        if(this.count<100){//ограничение в 99 пингов чтоб не поехала верстка графика
+          this.next();
+        }else{
+          this.abort();
+        }
+      }catch(error){
+        this.abort();
+        console.warn('ping.error',error);
+      };
+    },
+    start(){
+      if(!this.running&&this.valid){
+        this.enable=true;
+      };
+      this.next();
+    },
+    next(){
+      if(!this.running&&this.enable){
+        this.timer=setTimeout(this.ping,this.timeout);
+      };
+    },
+    abort(){
+      clearTimeout(this.timer);
+      this.enable=false;
+      this.running=false;
+    },
+    clear(){
+      this.abort();
+      this.count=0;
+      this.pings=[];
+    },
+    close(){//public
+      this.abort();
+    },
+  },
+  beforeDestroy() {
+    this.abort();
   },
 });
