96a67e4c8c41cb3bde04380499cd5d5a745fe63f
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2023-12-22 07:16:02 +0700

    Update SideBarMenuItemWfmVacantTaskWeb.js

--- SideBarMenuItemWfmVacantTaskWeb.js
+++ SideBarMenuItemWfmVacantTaskWeb.js
@@ -1,10 +1,19 @@
 Vue.component('SideBarMenuItemWfmVacantTaskWeb',{
-  template:`<div class="display-flex align-items-center gap-8px cursor-pointer" @click="onClick" v-if="!isB2BEngineer">
+  template:`<div class="display-contents">
+  <div class="display-flex align-items-center gap-8px cursor-pointer" @click="$store.dispatch('menu/close')" v-if="!isB2BEngineer">
+    <IcIcon name="fas fa-link" color="#5642bd" class="font-size-22px"/>
+    <span>Биржа нарядов</span>
+    <div class="margin-left-auto">
+      <IcIcon name="fa fa-chevron-right"/>
+    </div>
+  </div>
+  <div class="display-flex align-items-center gap-8px cursor-pointer" @click="onClick" v-if="!isB2BEngineer">
     <IcIcon name="fas fa-link" color="#5642bd" class="font-size-22px"/>
     <span>Биржа нарядов (2)</span>
     <div class="margin-left-auto">
       <IcIcon name="fa fa-chevron-right"/>
     </div>
+  </div>
   </div>`,
   computed:mapGetters(['userLogin','isB2BEngineer']),
   methods:{
