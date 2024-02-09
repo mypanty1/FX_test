5164f839b3e4a4574a876eeb39a12574f21a875c
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-02-09 15:11:56 +0700

    Update FIX_test_21.04.23.js

--- FIX_test_21.04.23.js
+++ FIX_test_21.04.23.js
@@ -172,7 +172,7 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 //fix edit descr
 //document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteNodeDetails.js',type:'text/javascript'}));
 //fix cm filter
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CM.TYPES.js',type:'text/javascript'}));
+//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CM.TYPES.js',type:'text/javascript'}));
 //add opt 82 session port for nsk
 //document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortActionMac_PortEntitiesByMac.js',type:'text/javascript'}));
 //add rack index
