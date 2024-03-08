917eca0f25f18fe07e1d3d351e707b02b999e2b7
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-03-08 12:54:04 +0700

    Update FIX_test_21.04.23.js

--- FIX_test_21.04.23.js
+++ FIX_test_21.04.23.js
@@ -172,12 +172,11 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 //fix edit descr
 //document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteNodeDetails.js',type:'text/javascript'}));
 
-//test task nav deeplink
-//if(store?.getters?.userLogin=='mypanty1'){
-  document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CMTaskSiteAddress.js',type:'text/javascript'}));
-//};
-//fix iptv creds
-//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/LbsvService.js',type:'text/javascript'}));
+//add nav deeplinks
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CMTaskSiteAddress.js',type:'text/javascript'}));
+//fix status button, add ls, and button gap
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CMTaskInfo_CMTaskPotentialAccount_CMTaskAssignment.js',type:'text/javascript'}));
+
 //add rack index
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/device-info.js',type:'text/javascript'}));
 
