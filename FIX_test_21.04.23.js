7ede309046f15ef768714025ba9233ef096c4735
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-03-08 13:17:23 +0700

    Update FIX_test_21.04.23.js

--- FIX_test_21.04.23.js
+++ FIX_test_21.04.23.js
@@ -175,7 +175,7 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 //add nav deeplinks
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CMTaskSiteAddress_WFMTaskSiteAddress_task-main-account-2_task-main-incident.js',type:'text/javascript'}));
 //fix status button, add ls, and button gap
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CMTaskInfo_CMTaskPotentialAccount_CMTaskAssignment.js',type:'text/javascript'}));
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CMTaskCard_CMTaskInfo_CMTaskPotentialAccount_CMTaskAssignment.js',type:'text/javascript'}));
 
 //add rack index
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/device-info.js',type:'text/javascript'}));
