be0edf448f6738086ad2cfa49e1047f2fe126e47
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-03-20 09:28:14 +0700

    Update FIX_test_21.04.23.js

--- FIX_test_21.04.23.js
+++ FIX_test_21.04.23.js
@@ -178,8 +178,8 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 //add cpe speedtest
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SpeedTest.js',type:'text/javascript'}));
 
-//add rack index
-//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/device-info.js',type:'text/javascript'}));
+//fix error message
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/CMTaskStatusChangeModal.js',type:'text/javascript'}));
 //fix multi node points
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/Buildings.js',type:'text/javascript'}));
 
