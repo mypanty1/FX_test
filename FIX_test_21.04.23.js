32a88f896bacac5c67dcf5f773d7eefd9031d624
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-04-22 12:59:36 +0700

    Update FIX_test_21.04.23.js

--- FIX_test_21.04.23.js
+++ FIX_test_21.04.23.js
@@ -302,7 +302,9 @@ createStyleElement('watermark-css',`
     justify-content:center;
     font-size:30px;
     line-height:30px;
-    font-weight: bold;
+    //font-size:28px;
+    //line-height:28px;
+    //font-weight: bold;
     opacity:0.007;
     pointer-events:none;
     z-index:99988;
