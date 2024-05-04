30bdcfe14009d2d5fea6380d5de72264aae8bfa0
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-05-04 13:36:39 +0700

    Update FIX_test_21.04.23.js

--- FIX_test_21.04.23.js
+++ FIX_test_21.04.23.js
@@ -183,160 +183,155 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/EngineerPage.js',type:'text/javascript'}));
 
 
-/*
-const GoBillingServiceLbsv = new class GoBillingServiceLbsv extends GoRequestService {
-  getVgroupEx(serverID, vgID, ActivTaskNumber, ServiceType) {
-    return this.post('GetVgroupEx', this.mergeParams({
-      serverid: serverID,
-      vgroupid: vgID
-    }), this.metaDataHeaders({ActivTaskNumber, ServiceType}));
-  }
-}('/v1/BillingService/Lbsv');
+Vue.component('SiteNodeDetails', {
+  components: {
+    SiteLastTechMaintenance: () => import('/fe/ma/features/TechMaintenance/components/SiteLastTechMaintenance.js'),
+  },
+  template:`<div class="white-block-100 padding-top-bottom-8px display-flex flex-direction-column gap-8px">
+    <div class="padding-left-12px">
+      <title-main text="Инфо по площадке*" @open="show=!show" class="padding-unset">
+        <button-sq :icon="loading ? 'loading rotating' : 'mark-circle'" @click="help.show = !help.show"/>
+        <button-sq v-if="show && siteNode" icon="edit" @click="$refs.SiteNodeDetailsEditModal.open()" :disabled="loading"/>
+      </title-main>
+      <SiteNodeDetailsEditModal v-if="siteNode" ref="SiteNodeDetailsEditModal" v-bind="modalProps" @onNodeSaveOk="get_nioss_object('node',siteNode?.node_id,'update')" @onSiteSaveOk="get_nioss_object('site',siteNode?.id,'update')"/>
+    </div>
+    
+    <div class="padding-left-right-12px">
+      <info-text-icon v-if="help.show" icon="info" :text="help.text" class="padding-unset"/>
+    </div>
+    
+    <template v-if="show && siteNode">
+      <div v-if="siteNode.lessor" class="padding-left-right-12px display-flex flex-direction-column gap-4px">
+        <div class="font--12-400">{{siteNode.lessor.name}}</div>
+        <PhoneCall v-if="siteNode.lessor.phone" :phone="siteNode.lessor.phone" title="Контактный номер телефона" :descr="[siteNode.lessor.person,siteNode.lessor.position]"/>
+        <PhoneCall v-if="siteNode.lessor.phone2" :phone="siteNode.lessor.phone2" title="Контактный номер телефона по вопросам доступа"/>
+        <PhoneCall v-if="siteNode.lessor.phone3" :phone="siteNode.lessor.phone3" title="Телефонные номера аварийных служб"/>
+      </div>
+      
+      <div class="divider-line"/>
 
-[GoOctopusService, GoRemedyWorkService, GoNiossService, GoDeviceServiceDnm, GoSiebelService, GoMangoService, GoSwitchRep, GoEmailer, GoMaintenance, GoBillingServiceLbsv].forEach(service=>{
-  service.metaDataHeaders = function(metaData = {}, headers = {}){
-    return {
-      ...headers,
-      'Grpc-Metadata-X-Log': Object.entries({
-        ...metaData,
-        'InetcoreUserLogin': store.getters.userLogin,
-        'InetcoreUserRegionID': store.getters.regionID,
-        'InetcoreUserPosition': (store.getters['main/lastCoords'] || store.getters.respawn || DEFAULT_COORDS).map(c => parseFloat(c).toFixed(5)).join('/'),
-        'InetcoreUserPlatform': window.navigator.platform,
-        'InetcoreUserConnection': [window.navigator.connection.effectiveType, window.navigator.connection.downlink].join('/'),
-      }).map(([key, value]) => (`${key}=${value}`)).join(',')
-    }
-  }
-});
+      <div class="padding-left-right-12px">
+        <div class="font--13-500 tone-500">{{address_descr_title}}</div>
+        <div class="font--12-400">{{address_descr}}</div>
+      </div>
+      
+      <div class="divider-line"/>
 
-Vue.mixin({
-  beforeCreate(){
-    if(this.$options.name=='MacEntityItem'){
-      this.$options.template=`<div class="display-flex margin-left-right-16px align-items-center gap-2px" @click="$emit('onClick',onClick())">
-        <div class="font--12-400 white-space-pre"><slot name="label">{{label}}:</slot></div>
-        <slot name="content">
-          <div class="font--13-500 white-space-pre padding-left-right-4px border-radius-4px" :class="textBgClass">{{text}}</div>
-        </slot>
-        <slot name="arrow">
-          <div v-if="showArrow" class="ic-20 ic-right-1 main-lilac bg-main-lilac-light border-radius-4px cursor-pointer"></div>
-        </slot>
-      </div>`
-    };
-    if(this.$options.name=='AbonPortBindSearchAbon'){
-      this.$options.methods.updateLbsvResource = async function({serverid,vgid}={}){
-        if(!serverid||!vgid){return};
-        const {vgroups}=this.lbsv;
-        try{
-          const serviceType = vgroups?.find(vg => vg.vgid === vgid)?.type;
-          const response = await GoBillingServiceLbsv.getVgroupEx(serverid, vgid, this.activeWfmTask?.NumberOrder, serviceType);
-          if(!response?.data?.vgid){return};
-          const currentVg=vgroups.find(vg=>vg.vgid==vgid);
-          if(!currentVg){return};
-          const currentVgIndex=vgroups.findIndex(vg=>vg.vgid==vgid);
-          this.$set(this.lbsv.vgroups,currentVgIndex,{
-            ...currentVg,
-            ...response.data
-          });
-        }catch(error){
-          console.warn(error);
-        }
+      <div class="padding-left-right-12px">
+        <div class="font--13-500 tone-500">{{site_descr_title}}</div>
+        <div class="font--12-400">{{site_descr}}</div>
+      </div>
+      
+      <div class="divider-line"/>
+
+      <div class="padding-left-right-12px">
+        <div class="font--13-500 tone-500">{{node_descr_title}}</div>
+        <div class="font--12-400">{{node_descr}}</div>
+      </div>
+      
+      <template v-if="!1&&siteID">
+        <div class="divider-line"/>
+
+        <div class="padding-left-right-12px">
+          <SiteLastTechMaintenance v-bind="{siteID}"/>
+        </div>
+      </template>
+      
+      <template v-if="address_id">
+        <div class="divider-line"/>
+
+        <UrlLink :url="urlToInventory"/>
+      </template>
+    </template>
+  </div>`,
+  props:{
+    siteNode:{type:Object},
+  },
+  data:()=>({
+    show:true,
+    help:{
+      text:`Информация об арендодателе площадей под размещение оборудования ПАО МТС может быть устаревшей либо вовсе не быть информацией по доступу. 
+      Для корректировки данной информации нужно обратиться к ФГТСЖ. Подробная информация по доступу в помещения подъезда находится на странице Подъезд`,
+      show:false,
+    },
+    resps:{//8100749217013993313 - получены все доступные атрибуты
+      node:null,
+      site:null,
+      address:null,
+    },
+    loads:{
+      node:false,
+      site:false,
+      address:null,
+    },
+  }),
+  created(){
+    this.get_nioss_object('address',this.siteNode?.address_id);
+    this.get_nioss_object('site',this.siteNode?.id);
+    this.get_nioss_object('node',this.siteNode?.node_id||this.siteNode?.uzel_id);
+  },
+  watch:{
+    'siteNode'(siteNode){
+      if(!siteNode){return};
+      if(!this.resps.site&&!this.loads.site){
+        this.get_nioss_object('site',this.siteNode?.id);
       }
-    }else if(this.$options.name=='LbsvContent' || this.$options.name=='LbsvB2bContent'){
-      this.$options.methods.updateAgreementServices = async function(){
-        const {serverID}=this;
-        // this.agreementServicesUpdated={};
-        for (const service of Object.values(this.agreementServices)) {
-          try{
-            const response = await GoBillingServiceLbsv.getVgroupEx(serverID, service.vgid, this.activeWfmTask?.NumberOrder, service.type);
-            if(response?.data?.vgid){
-              this.$set(this.agreementServicesUpdated,response.data.vgid,response.data);
-            };
-          }catch(error){
-            console.warn(error);
-          };
+      if(!this.resps.node&&!this.loads.node){
+        this.get_nioss_object('node',this.siteNode?.node_id||this.siteNode?.uzel_id);
+      }
+    },
+    'address_id'(address_id){
+      if(address_id&&!this.resps.address&&!this.loads.address){
+        this.get_nioss_object('address',address_id);
+      }
+    }
+  },
+  computed:{
+    siteID(){return this.siteNode?.id},
+    loading(){return Object.values(this.loads).some(l=>l)},
+    address_descr(){return [this.resps.address?.description,this.siteNode?.details].filter(Boolean).join('\n')||'—'},
+    address_descr_title(){return `Примечание к адресу ${[this.resps.address?.BuildingType||this.resps.address?.BldType||'',this.resps.address?.resourceBusinessName||''].filter(Boolean).join(' ')}`},
+    site_descr(){return (this.resps.site?this.resps.site?.description:this.siteNode?.site_descr)||'—'},
+    site_descr_title(){return `Примечание к площадке ${this.siteNode?.name||''}`},
+    node_descr(){return (this.resps.node?this.resps.node?.description:this.siteNode?.node_descr)||'—'},
+    //node_descr_title(){return `Примечание к ООС ${this.siteNode?.type||''}`},
+    node_descr_title(){return `Примечание к ООС ${this.siteNode?.node||''}`},
+    address_id(){return this.resps.site?.AddressPA?.NCObjectKey||this.siteNode?.address_id||''},
+    site_name(){return this.resps.site?.SiteName||this.siteNode?.name||''},
+    urlToInventory(){return {title:`Инвентори площадки ${this.site_name}`,hideUrl:!0,url:`https://inventory.ural.mts.ru/tb/address_view.php?id_address=${this.address_id}`}},
+    modalProps(){
+      const {id:site_id,node_id}=this.siteNode;
+      const {site,node}=this.resps;
+      return {site,site_id,node,node_id}
+    },
+  },
+  methods:{
+    async get_nioss_object(object='unknown',object_id='',update=false){
+      if(!object_id){return};
+      if(!update){
+        const cache=this.$cache.getItem(`get_nioss_object/${object_id}`);
+        if(cache){
+          this.resps[object]=cache;
+          return;
         };
       }
-    }
-  }
-})
-
-app.$router.addRoutes([
-  {
-    path: '/VacantTasks',
-    name: 'R_VacantTasks',
-    component: Vue.component('VacantTasks', {
-      template: `<div class="display-flex flex-direction-column gap-8px">
-        <iframe src="https://auth.mgts.ru/vtf" style="width:100%;height:90vh;border:none;"/>
-      </div>`
-    }),
-  }
-]);
-Vue.component('SideBarMenuItemWfmVacantTaskWeb',{
-  template:`<div class="display-flex align-items-center gap-8px cursor-pointer" @click="onClick" v-if="isB2CEngineer">
-    <IcIcon name="fas fa-link" color="#5642bd" class="font-size-22px"/>
-    <span>Биржа нарядов</span>
-    <div class="margin-left-auto">
-      <IcIcon name="fa fa-chevron-right"/>
-    </div>
-  </div>`,
-  computed:mapGetters([
-    'userLogin',
-    'isB2CEngineer',
-    'vacantTaskWebURI'
-  ]),
-  methods:{
-    onClick(){
-      const _uri = this.vacantTaskWebURI;
-      const uri = /(auth.mgts.ru)/.test(_uri) ? `https://auth.mgts.ru/vtf` : _uri
-      if(this.$store.getters['app/isApp']){
-        this.$store.dispatch('app/actionView', uri);
-      }else{
-        window.open(uri, '_blank');
+      this.loads[object]=true;
+      const response=await this.get_nioss_object_and_save({object_id,object});
+      this.resps[object]=response||null;
+      this.loads[object]=false;
+    },
+    async get_nioss_object_and_save({object_id,object}){
+      try{
+        const response=await httpGet(buildUrl("get_nioss_object",{object_id,cache:!0},"/call/nioss/"),true);
+        if(response?.parent){this.$cache.setItem(`get_nioss_object/${object_id}`,response)};
+        return response;
+      }catch(error){
+        console.warn("get_nioss_object.error",{object_id},error);
       }
-      this.$store.dispatch('menu/close');
+      return null;
     },
   }
 });
-
-createStyleElement('watermark-css',`
-  #watermark {
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    inset: 0;
-    opacity: 0.007;
-    pointer-events: none;
-    z-index: 99988;
-  }
-  #watermark tt {
-    display: block;
-    font-weight: bold;
-    font-family: monospace;
-    font-size: 30px;
-    line-height: 28px;
-    text-align: center;
-    color: #000000;
-  }
-`);
-function setWatermark(values = []){
-  let watermark = document.querySelector('#watermark');
-  if(!watermark){
-    document.body.insertAdjacentHTML('beforeEnd', `<div id="watermark"></div>`)
-    watermark = document.querySelector('#watermark');
-    if(!watermark){return};
-  };
-  watermark.innerHTML = values.map((value) => `<tt>${value}</tt>`).join('').repeat(30);
-};
-setWatermark([store.getters.userLogin]);
-app.$router.afterEach((to, from)=>{
-  setWatermark([...Object.values(to.params).reduce((values, value)=>{
-    if(value && ['number', 'string'].includes(typeof value)){
-      values.add(value)
-    };
-    return values;
-  }, new Set([store.getters.userLogin]))]);
-});
-*/
 
 let sendStateTimer=null;
 let savePositionTimer=null;
