179b9bfc5e66cfbfbb599ad7d66f2b80e0eebe96
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-08-24 03:27:27 +0700

    Update addons.js

--- addons.js
+++ addons.js
@@ -9,13 +9,13 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 
 //SiteLinkChangeTraps
 // document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteLinkChangeTraps.js',type:'text/javascript'}));
-document.head.appendChild(Object.assign(document.createElement('script'), {src: 'https://ping54.ru/shared/dev/SiteLinkChangeTraps.js', type: 'text/javascript'}));
+//document.head.appendChild(Object.assign(document.createElement('script'), {src: 'https://ping54.ru/shared/dev/SiteLinkChangeTraps.js', type: 'text/javascript'}));
 
 //add port disable approve dialog
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortActionDisableApproveModal.js',type:'text/javascript'}));
 
 //fix check session
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/AbonPortBindForm.js',type:'text/javascript'}));
+//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/AbonPortBindForm.js',type:'text/javascript'}));
 
 
 
