dc156e75057f2686beb74ab3f81bb6190657809d
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-12-21 03:27:20 +0700

    Update addons.js

--- addons.js
+++ addons.js
@@ -5,14 +5,14 @@ document.head.appendChild(Object.assign(document.createElement('script'),{src:'h
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteNodeDuExt.js',type:'text/javascript'}));
 
 //SendKionPq
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SendKionPq.js',type:'text/javascript'}));
+//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SendKionPq.js',type:'text/javascript'}));
 
 //SiteLinkChangeTraps
 // document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteLinkChangeTraps.js',type:'text/javascript'}));
 //document.head.appendChild(Object.assign(document.createElement('script'), {src: 'https://ping54.ru/shared/dev/SiteLinkChangeTraps.js', type: 'text/javascript'}));
 
 //add port disable approve dialog
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortActionDisableApproveModal.js',type:'text/javascript'}));
+//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortActionDisableApproveModal.js',type:'text/javascript'}));
 
 //fix check session
 //document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/AbonPortBindForm.js',type:'text/javascript'}));
